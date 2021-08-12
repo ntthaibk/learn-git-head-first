d906ac09bbc7c4af45384adfd07cd358cf311ea7
ThaiNeeee <ntthaibk@gmail.com>
2021-08-13 00:45:38 +0700

    CONFLICT TIME

--- projectFolder/randomLearngit.java
+++ projectFolder/randomLearngit.java
@@ -10,7 +10,10 @@ public class RanDom{
 
 //learn git randomly
     //some changes made by me
-    public void randomForReal();
+    public void randomForReal();static String toString(Type type) {
+        return (type instanceof Class) ? ((Class<?>) type).getName() : type.toString();
+       }
+
 
 //learn git randomly
     //some changes made by me
@@ -21,6 +24,17 @@ public class RanDom{
 //learn git randomly
     //some changes made by me
     public void randomForReal();
+    @SuppressWarnings("unchecked")
+static Class<? extends Annotation> getAnnotationType(AnnotationFormatterFactory<? extends Annotation> factory) {
+  Class<? extends Annotation> annotationType = (Class<? extends Annotation>)
+      GenericTypeResolver.resolveTypeArgument(factory.getClass(), AnnotationFormatterFactory.class);
+  if (annotationType == null) {
+    throw new IllegalArgumentException("Unable to extract parameterized Annotation type argument from " +
+        "AnnotationFormatterFactory [" + factory.getClass().getName() +
+        "]; does the factory parameterize the <A extends Annotation> generic type?");
+  }
+  return annotationType;
+}
 
 //learn git randomly
     //some changes made by me
