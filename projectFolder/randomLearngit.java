8e954d70131361b9eafc27c332aeedf823aab614
ThaiNeeee <ntthaibk@gmail.com>
2021-08-13 00:45:44 +0700

    CONFLICT TIME

--- projectFolder/randomLearngit.java
+++ projectFolder/randomLearngit.java
@@ -16,6 +16,17 @@ public class RanDom{
 
 
 //learn git randomly
+@SuppressWarnings("unchecked")
+static Class<? extends Annotation> getAnnotationType(AnnotationFormatterFactory<? extends Annotation> factory) {
+  Class<? extends Annotation> annotationType = (Class<? extends Annotation>)
+      GenericTypeResolver.resolveTypeArgument(factory.getClass(), AnnotationFormatterFactory.class);
+  if (annotationType == null) {
+    throw new IllegalArgumentException("Unable to extract parameterized Annotation type argument from " +
+        "AnnotationFormatterFactory [" + factory.getClass().getName() +
+        "]; does the factory parameterize the <A extends Annotation> generic type?");
+  }
+  return annotationType;
+}
     //some changes made by me
 
 
