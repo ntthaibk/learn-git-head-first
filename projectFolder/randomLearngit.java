618efd0c2f8e441b4fef0207d80e1146f077e6f3
ThaiNeeee <ntthaibk@gmail.com>
2021-08-13 00:44:58 +0700

    CONFLICT TIME

--- projectFolder/randomLearngit.java
+++ projectFolder/randomLearngit.java
@@ -4,14 +4,41 @@ public class RanDom{
     public void randomForReal();
 
 //learn git randomly
-    //some changes made by me
+private boolean isMyServiceRunning(Class<?> serviceClass) {
+    ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+    for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+      if (serviceClass.getName().equals(service.service.getClassName())) {
+        return true;
+      }
+    }
+    return false;
+  }
 
     public void randomForReal();
 
 //learn git randomly
     //some changes made by me
     public void randomForReal();
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+          if (serviceClass.getName().equals(service.service.getClassName())) {
+            return true;
+          }
+        }
+        return false;
+      }
 
+
+      private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+          if (serviceClass.getName().equals(service.service.getClassName())) {
+            return true;
+          }
+        }
+        return false;
+      }
 //learn git randomly
     //some changes made by me
 
